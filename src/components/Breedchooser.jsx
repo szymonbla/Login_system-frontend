e4b770d0efeff0d9528cb4556aad9625bd34acf9
Kuchar <sz.blazynski@gmail.com>
2021-03-17 23:03:58 +0100

    Add Breedchooser

---
+++ src/components/Breedchooser.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Breedchooser = () => <h1>Slide</h1>;
+
+export default Breedchooser;
--- src/components/Createsetting.jsx
+++ src/components/Createsetting.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Slide from "../components/Slide";
 
 const Createsetting = () => (
   <div className="setting-container">
     <div className="setting-nickname"></div>
-    <div className="setting-limit"></div>
+    <div className="setting-limit">
+      <Slide />
+    </div>
     <div className="setting-breed"></div>
   </div>
 );

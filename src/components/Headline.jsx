3e47ea5064e9b2632e97398373f1b36bfc30568b
Kuchar <sz.blazynski@gmail.com>
2021-03-17 20:46:45 +0100

    Add content

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Headline = () => (
   <div className="headline-container">
-    <span className="headline-header">Rock, Paper, Scissors</span>
+    <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+      <span className="headline-header">Rock, Paper, Scissors</span>
+    </Link>
     <div className="headline-description">
       <span className="headline-description-title">Welcome to</span>
       <div className="headline-description-content">Web application which facilities you to settle all disputes</div>
--- src/pages/Createbattle.jsx
+++ src/pages/Createbattle.jsx
@@ -1,7 +1,18 @@
 import react from "react";
+import Headline from "../components/Headline";
 
 const Createbattle = () => {
-  return <h1>Create battle</h1>;
+  return (
+    <div className="create-container">
+      <div className="create-header">
+        <Headline />
+      </div>
+      <div className="create-content">
+        <div className="column"></div>
+        <div className="column"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Createbattle;

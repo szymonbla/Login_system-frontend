9505ed49235f836f69b99134fbc9c67aa52b049d
Kuchar <sz.blazynski@gmail.com>
2021-03-19 14:51:47 +0100

    Add axios register method

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import Headline from "../components/Headline";
+import Axios from "axios";
 
 const Register = () => {
   const [userName, setUserName] = useState("");
   const [mail, setMail] = useState("");
-  const [passwrod, setPassword] = useState("");
+  const [passwordReq, setPassword] = useState("");
+
+  const register = () => {
+    Axios.post("http://localhost:5000/register", {
+      username: userName,
+      email: mail,
+      password: passwordReq,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
 
   return (
     <div className="register-container">
@@ -16,7 +27,7 @@ const Register = () => {
           <input type="text" placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
           <input type="email" placeholder="Email" onChange={(e) => setMail(e.target.value)} />
           <input type="password" placeholder="" onChange={(e) => setPassword(e.target.value)} />
-          <button>Register</button>
+          <button onClick={register}>Register</button>
         </form>
       </div>
     </div>

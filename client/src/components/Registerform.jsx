29b6fc00eb6a93f8a2c170b8eb2cf37c507027a4
Kuchar <sz.blazynski@gmail.com>
2021-03-19 18:25:21 +0100

    Refactor file structure

--- client/src/components/Registerform.jsx
+++ client/src/components/Registerform.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import Button from "../components/Button";
+import Button from "./Utils/Button";
 
 const Registerform = () => {
   const [userName, setUserName] = useState("");

5af689ae0d4e60a3aa6cbcf1f7c032aadb216c1c
Kuchar <sz.blazynski@gmail.com>
2021-03-24 19:32:07 +0100

    Socket io works fine

--- client/src/pages/Battle.jsx
+++ client/src/pages/Battle.jsx
@@ -9,13 +9,13 @@ const Battle = ({ location }) => {
   const ENDPOINT_TO_BACKEND = "http://localhost:5000/";
 
   useEffect(() => {
-    const data = queryString.parse(location.search);
+    const { tag } = queryString.parse(location.search);
     socket = io(ENDPOINT_TO_BACKEND);
 
-    setName(location.search);
-    console.log(data);
+    setName(tag);
+    console.log(tag);
     console.log(socket);
-  });
+  }, [ENDPOINT_TO_BACKEND]);
 
   return <h1>Battle page</h1>;
 };

6a751061de9a0f66a8550ada5e04beb43a190753
Szymon <sz.blazynski@gmail.com>
2021-03-26 14:09:22 +0100

    Add connection with API and token validation

--- client/src/pages/Battle.jsx
+++ client/src/pages/Battle.jsx
@@ -1,28 +1,50 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string"; // It helps to retrive data from the URL
-import io from "socket.io-client";
+import * as io from "socket.io-client";
+import axios from "axios";
 
 let socket;
 
 const Battle = ({ location }) => {
   const [name, setName] = useState("");
+  const [user, setUser] = useState([]);
   const ENDPOINT_TO_BACKEND = "http://localhost:5000/";
 
-  useEffect(() => {
+  const reqApi = async () => {
+    await axios
+      .get("http://localhost:5000/app/battle", {
+        headers: {
+          "x-access-token": localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setUser(JSON.stringify(response.data.user));
+      });
+  };
+
+  useEffect(async () => {
     const { tag } = queryString.parse(location.search);
     socket = io(ENDPOINT_TO_BACKEND);
 
+    await reqApi();
     setName(tag);
     console.log(tag);
     console.log(socket);
-
     return () => {
       socket.on("disconnect");
       socket.off();
     };
   }, [ENDPOINT_TO_BACKEND]);
 
-  return <h1>Battle page</h1>;
+  useEffect(() => {
+    socket.emit("data", { name, user });
+  }, [user]);
+
+  return (
+    <h1>
+      Battle page {name}, {user}
+    </h1>
+  );
 };
 
 export default Battle;
